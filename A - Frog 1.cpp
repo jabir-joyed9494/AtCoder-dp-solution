2d4a92959f7600786108b58a4865bfe4b22cfe6b
jabir-joyed9494 <71986259+jabir-joyed9494@users.noreply.github.com>
2024-01-17 01:21:16 +0600

    Create A - Frog 1.cpp

---
+++ A - Frog 1.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+ll aray[100000+5];
+vector<ll>dp(100000+5,-1);
+ll mincost(ll n)
+{
+    if(n==1) return 0;
+    if(dp[n]!=-1) return dp[n];
+    ll minn=INT_MAX;
+    if(n>0) minn=min(minn, mincost(n-1)+abs(aray[n]-aray[n-1]));
+    if(n>1) minn=min(minn, mincost(n-2)+abs(aray[n]-aray[n-2]));
+    return dp[n]=minn;
+}
+int main()
+{
+
+    int t=1;
+    // cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        for(int i=1; i<=n; i++)
+            cin>>aray[i];
+        //memset(dp,-1,sizeof dp);
+        ll ans=mincost(n);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+
+
+
